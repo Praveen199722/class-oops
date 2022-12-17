2d9e08600d0a1e1aceb4d41b4f7be0f0060c54b6
praveen kumar.m <118429551+Praveen199722@users.noreply.github.com>
2022-12-17 09:02:59 +0530

    Add files via upload

---
+++ function assignment-1 githhub.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#create function
+
+
+# In[ ]:
+
+
+subfields()
+
+
+# In[13]:
+
+
+def subfields():
+    print(""""subfields in AI are:
+    machine learning
+    neutral networks
+    vision
+    robotics
+    speech processing
+    natural language processing""")
+
+
+# In[14]:
+
+
+subfields()
+
+
+# In[2]:
+
+
+def oddeven():
+    num=int(input("enter the number:"))
+    if((num%2)==0):
+        print("even number")
+        message="even number"
+    else:
+        print("odd number")
+    message="odd number"
+    return message
+
+
+# In[3]:
+
+
+oddeven()
+
+
+# In[4]:
+
+
+#create function
+
+
+# In[29]:
+
+
+def elegible():
+    age=(input("your gender:"))
+    message="your gender"
+    age=int(input("your age:"))
+    if(age<=18):
+        print("not eligible")
+        message="not eligibe"
+    elif(age>18):
+        print("eligible")
+        message="eligible"
+    else:
+        print("eligible")
+        return elegible
+
+
+# In[30]:
+
+
+elegible()
+
+
+# In[ ]:
+
+
+#create function
+
+
+# In[53]:
+
+
+def percentage():
+    a=int(input("subject1="))
+    message="subject1"
+    b=int(input("subject2="))
+    message="subject2"
+    c=int(input("subject3="))
+    message="subject3"
+    d=int(input("subject4="))
+    message="subject4"
+    e=int(input("subject5="))
+    message="subject5"
+    print("total=",a+b+c+d+e)
+    total=a+b+c+d+e
+    percentage=total/5
+    print("percentage:",percentage)
+    return message
+
+    
+
+
+# In[54]:
+
+
+percentage()
+
+
+# In[ ]:
+
+
+#create function
+
+
+# In[ ]:
+
+
+triangle()
+
+
+# In[4]:
+
+
+def triangle():
+    a=int(input("height"))
+    b=int(input("breadth"))
+    area=(a*b/2)
+    print("AREA OF TRIANGLE:",area)
+    a1=int(input("height1:"))
+    b1=int(input("height2:"))
+    c1=int(input("breadth:"))
+    perimeter=a1+b1+c1
+    print("perimeter of triangle:",perimeter)
+    
+
+
+# In[5]:
+
+
+triangle()
+
+
+# In[ ]:
+
+
+
+
