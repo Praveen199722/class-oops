abd7d6ec20b09839d38d094041391607d729cd92
praveen kumar.m <118429551+Praveen199722@users.noreply.github.com>
2022-12-17 08:58:23 +0530

    Add files via upload

---
+++ class finished.AI.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from ClassMultipleFunction import subfieldsInAi
+
+
+# In[2]:
+
+
+subfieldsInAi.oddeven()
+
+
+# In[3]:
+
+
+subfieldsInAi.percentage()
+
+
+# In[4]:
+
+
+subfieldsInAi.elegible()
+
+
+# In[5]:
+
+
+subfieldsInAi.triangle()
+
+
+# In[ ]:
+
+
+
+
